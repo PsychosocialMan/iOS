1bcb4129ffcf9e7a5c48a9d1a6fc2fc73835a04b
Kiril Smirnov <kirill-sm2009@yandex.ru>
2018-10-07 20:16:27 +0300

    Initial Commit

---
+++ dz2/IphoneCell.swift
@@ -0,0 +1,24 @@
+//
+//  IphoneCell.swift
+//  dz2
+//
+//  Created by Борис Голубков on 07.10.2018.
+//  Copyright © 2018 StudentsParty. All rights reserved.
+//
+
+import UIKit
+
+class IphoneCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
